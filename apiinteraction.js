3ab0196348d31bc8f08830cf875af742f01a9e12
Jordan Crawford <jordan.crawford@me.com>
2016-04-23 15:20:50 +1200

    Setup to send data to real API.

--- apiinteraction.js
+++ apiinteraction.js
@@ -4,6 +4,9 @@
 	Handles all API interaction.
 */
 
+var request = require('request');
+var dateFormat = require('dateformat')
+
 var APIInteraction = function(configuration, logContext) {
 	this.configuration = configuration;
 	this.logContext = logContext;
@@ -13,6 +16,23 @@ var APIInteraction = function(configuration, logContext) {
 	// TODO: queue requests up then send.
 APIInteraction.prototype.sendSensorData = function(sensorId, sensorValue) {
 	this.logContext.log('Got sensor data back. id: ' + sensorId + ', value: ' + sensorValue);
+
+	var timestamp = dateFormat(new Date(), 'isoDateTime');
+	request.post(this.configuration.apiEndpoint + '/Readings',
+		{
+			sensorId : sensorId,
+			value : sensorValue,
+			timestamp : timestamp
+		}, function(error, response, body) {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log('res');
+				console.log(response);
+				console.log('body');
+				console.log(body);
+			}
+		});
 };
 
 module.exports = APIInteraction;
