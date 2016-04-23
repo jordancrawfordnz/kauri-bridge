24d3300794405af5592c914322525f6839a88bc1
Jordan Crawford <jordan.crawford@me.com>
2016-04-23 15:13:22 +1200

    Fix API interaction.

--- apiinteraction.js
+++ apiinteraction.js
@@ -7,12 +7,12 @@
 var APIInteraction = function(configuration, logContext) {
 	this.configuration = configuration;
 	this.logContext = logContext;
-
-	console.log(configuration);
 };
 
 // Sends sensor data to the API.
 	// TODO: queue requests up then send.
 APIInteraction.prototype.sendSensorData = function(sensorId, sensorValue) {
-	logContext.log('Got sensor data back. id: ' + sensorId + ', value: ' + sensorValue);
+	this.logContext.log('Got sensor data back. id: ' + sensorId + ', value: ' + sensorValue);
 };
+
+module.exports = APIInteraction;
